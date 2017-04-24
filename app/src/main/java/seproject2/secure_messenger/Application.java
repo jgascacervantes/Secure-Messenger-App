b06b5f78ea8245046f523dc4c6c7647b6b4a6b35
dukeofdank <jnbunting9265@gmail.com>
2017-04-23 21:37:26 -0500

    App now registers device to receive messages

--- app/src/main/java/seproject2/secure_messenger/Application.java
+++ app/src/main/java/seproject2/secure_messenger/Application.java
@@ -4,6 +4,7 @@ import android.support.multidex.MultiDexApplication;
 import android.util.Log;
 
 import com.amazonaws.mobile.AWSMobileClient;
+import com.amazonaws.mobile.push.PushManager;
 
 public class Application extends MultiDexApplication {
     private final static String LOG_TAG = Application.class.getSimpleName();
@@ -20,6 +21,15 @@ public class Application extends MultiDexApplication {
 
         // Initialize the AWS Mobile Client
         AWSMobileClient.initializeMobileClientIfNecessary(getApplicationContext());
+
+        // Set a listener for changes in push notification state
+        PushManager.setPushStateListener(new PushManager.PushStateListener() {
+            @Override
+            public void onPushStateChange(final PushManager pushManager, boolean isEnabled) {
+                Log.d(LOG_TAG, "Push Notifications Enabled = " + isEnabled);
+                // ...Put any application-specific push state change logic here...
+            }
+        });
 
         // ... Put any application-specific initialization logic here ...
     }
--- app/src/main/java/seproject2/secure_messenger/MainActivity.java
+++ app/src/main/java/seproject2/secure_messenger/MainActivity.java
@@ -1,19 +1,27 @@
 package seproject2.secure_messenger;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.content.Intent;
+
+import com.amazonaws.mobile.AWSMobileClient;
+import com.amazonaws.mobile.push.PushManager;
+
+import com.amazonaws.AmazonClientException;
 
 import seproject2.secure_messenger.dummy.DummyContent;
 
 public class MainActivity extends AppCompatActivity implements InboxFragment.OnListFragmentInteractionListener {
 
     private static FloatingActionButton button_X;
+    private PushManager pushManager;
 
 
     View.OnClickListener mOnClickListener;
@@ -34,6 +42,28 @@ public class MainActivity extends AppCompatActivity implements InboxFragment.OnL
             }
         });
         //ShowContactsListener();
+
+        pushManager = AWSMobileClient.defaultMobileClient().getPushManager();
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(final Void... params) {
+                // register device first to ensure we have a push endpoint.
+                pushManager.registerDevice();
+
+                // if registration succeeded.
+                if (pushManager.isRegistered()) {
+                    try {
+                        pushManager.setPushEnabled(true);
+                        // Automatically subscribe to the default SNS topic
+                        pushManager.subscribeToTopic(pushManager.getDefaultTopic());
+                        return null;
+                    } catch (final AmazonClientException ace) {
+                        return ace.getMessage();
+                    }
+                }
+                return "Failed to register for push notifications.";
+            }
+        }.execute();
 
 
         mOnClickListener = new View.OnClickListener() {
